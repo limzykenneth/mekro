008871b24cb4d784fe259cc8d4515b3a7926e38c
limzykenneth <limzy.kenneth@gmail.com>
2021-07-12 22:34:22 +0100

    Little bit of interaction

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,6 +1,9 @@
 pub mod configuration{
 	use serde::{Serialize, Deserialize};
-	use tui::widgets::ListState;
+	use tui::{
+		widgets::{ListState, ListItem},
+		style::{Style},
+	};
 	use serde_json;
 
 	#[derive(Serialize, Deserialize, Debug)]
@@ -10,17 +13,60 @@ pub mod configuration{
 	}
 
 	pub struct Configurations<'a>{
-		items: Vec<Configuration<'a>>,
-		state: ListState
+		pub items: Vec<ListItem<'a>>,
+		pub state: ListState
 	}
 
-	// impl Configurations{
-	// 	fn new(config: &str) -> Configurations{
-	// 		let parse_configuration(config)
-	// 	}
-	// }
+	impl Configurations<'_>{
+		pub fn new(config: &str) -> Configurations{
+			let values = parse_configuration(config);
+			let items: Vec<ListItem> = values.iter()
+				.map(|v| {
+					ListItem::new(v.command)
+						.style(Style::default())
+				})
+				.collect();
 
-	// fn parse_configuration(data: &str) -> Vec<Configuration> {
-	// 	serde_json::from_str(data).unwrap()
-	// }
+			Configurations{
+				items,
+				state: ListState::default()
+			}
+		}
+
+		pub fn next(&mut self) {
+			let i = match self.state.selected() {
+				Some(i) => {
+					if i >= self.items.len() - 1 {
+						0
+					} else {
+						i + 1
+					}
+				}
+				None => 0,
+			};
+			self.state.select(Some(i));
+		}
+
+		pub fn previous(&mut self) {
+			let i = match self.state.selected() {
+				Some(i) => {
+					if i == 0 {
+						self.items.len() - 1
+					} else {
+						i - 1
+					}
+				}
+				None => 0,
+			};
+			self.state.select(Some(i));
+		}
+
+		pub fn unselect(&mut self) {
+			self.state.select(None);
+		}
+	}
+
+	fn parse_configuration(data: &str) -> Vec<Configuration> {
+		serde_json::from_str(data).unwrap()
+	}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
 use std::process::Command;
-use serde_json;
 use std::fs;
 use std::io;
 use tui::{
-    backend::CrosstermBackend,
-    layout::{Constraint, Corner, Direction, Layout},
-    style::{Color, Modifier, Style},
-    text::{Span, Spans},
-    widgets::{Block, Borders, List, ListItem},
-    Terminal
+	backend::CrosstermBackend,
+	layout::{Constraint, Corner, Direction, Layout},
+	style::{Color, Modifier, Style},
+	text::{Span, Spans},
+	widgets::{Block, Borders, List, ListItem},
+	Terminal
 };
+use std::time::Duration;
+use crossterm::event::{poll, read, Event, KeyCode, KeyEvent, KeyModifiers};
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
 
 mod configuration;
-use configuration::configuration::Configuration;
+use configuration::configuration::Configurations;
 
 fn main() -> Result<(), io::Error> {
 	let mut child = Command::new("ls")
@@ -27,21 +29,44 @@ fn main() -> Result<(), io::Error> {
 	let contents = fs::read_to_string("./example.json")
 		.expect("Something went wrong reading the file");
 
-	let values = parse_configuration(&contents);
-
-	println!("{:?}", values);
 	let stdout = io::stdout();
 	let backend = CrosstermBackend::new(stdout);
 	let mut terminal = Terminal::new(backend)?;
 
+	let mut configurations = Configurations::new(&contents);
+
+	enable_raw_mode().unwrap();
 	loop{
+		if poll(Duration::from_millis(100)).unwrap() {
+			// It's guaranteed that `read` wont block, because `poll` returned
+			// `Ok(true)`.
+			match read().unwrap(){
+				Event::Key(KeyEvent {
+					code: KeyCode::Char('c'),
+					modifiers: KeyModifiers::CONTROL
+				}) => break,
+				Event::Key(KeyEvent {
+					code: KeyCode::Down,
+					modifiers: KeyModifiers::NONE
+				}) => {
+					// println!("Down");
+					configurations.next();
+				},
+				Event::Key(KeyEvent {
+					code: KeyCode::Up,
+					modifiers: KeyModifiers::NONE
+				}) => {
+					// println!("Up");
+					configurations.previous();
+				},
+				_ => ()
+			};
+		} else {
+			// Timeout expired, no `Event` is available
+		}
+
 		terminal.draw(|f| {
-			let items: Vec<ListItem> = values.iter()
-				.map(|v| {
-					ListItem::new(v.command)
-						.style(Style::default())
-				})
-				.collect();
+			let items = configurations.items.clone();
 
 			let list = List::new(items)
 				.block(Block::default()
@@ -51,15 +76,14 @@ fn main() -> Result<(), io::Error> {
 					.fg(Color::Black))
 				.highlight_style(Style::default()
 					.bg(Color::LightGreen)
-                    .add_modifier(Modifier::BOLD))
+					.add_modifier(Modifier::BOLD))
 				.highlight_symbol(">>");
 
 			let size = f.size();
-			f.render_widget(list, size);
+			f.render_stateful_widget(list, size, &mut configurations.state);
 		})?;
 	}
-}
 
-fn parse_configuration(data: &str) -> Vec<Configuration> {
-	serde_json::from_str(data).unwrap()
+	disable_raw_mode().unwrap();
+	Ok(())
 }
