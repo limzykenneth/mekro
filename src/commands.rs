99eedb52ff219065a55f86f1c251ac447bc20600
limzykenneth <limzy.kenneth@gmail.com>
2021-07-14 00:09:07 +0100

    Probably enough for getting output from tokio child processes

--- src/commands.rs
+++ src/commands.rs
@@ -3,43 +3,71 @@ pub mod commands{
 		widgets::{ListState, ListItem},
 		style::{Style},
 	};
-	use tokio::process::{Command as Cmd, Child};
+	use tokio::process::{
+		Command as Cmd,
+		Child,
+		ChildStdout
+	};
+	use tokio::io::{BufReader, AsyncBufReadExt, Lines};
+	use std::process::Stdio;
 	use crate::configuration::configuration::{
 		Configuration,
 		parse_configuration
 	};
 
+	#[derive(Debug)]
 	pub struct Command<'a>{
-		pub item: ListItem<'a>,
 		pub child_process: Option<Child>,
+		pub stdout: Option<Lines<BufReader<ChildStdout>>>,
 		command: &'a str,
-		arguments: Vec<&'a str>
+		arguments: Vec<&'a str>,
+		pub output: String
 	}
 
 	impl Command<'_>{
 		fn new<'a>(config: &Configuration<'a>) -> Command<'a>{
 			Command {
-				item: ListItem::new(config.command)
-					.style(Style::default()),
 				child_process: None,
+				stdout: None,
 				command: config.command,
-				arguments: config.arguments.clone()
+				arguments: config.arguments.clone(),
+				output: String::from("")
 			}
 		}
 
-		fn run(&mut self){
-			let child = Cmd::new(self.command)
-				.args(&self.arguments)
+		async fn run(&mut self){
+			let mut cmd = Cmd::new(self.command);
+			cmd.stdout(Stdio::piped());
+			cmd.args(&self.arguments);
+
+			let mut child = cmd
 				.spawn()
 				.expect("Failed to execute command");
 
+			let stdout = child.stdout.take()
+				 .expect("child did not have a handle to stdout");
+
+			let mut stdout = BufReader::new(stdout).lines();
 			self.child_process = Some(child);
+
+			// tokio::spawn(async move {
+			// 	let status = child.wait().await
+			// 		.expect("child process encountered an error");
+
+			// 	println!("child status was: {}", status);
+			// });
+
+			while let Some(line) = stdout.next_line().await.unwrap() {
+				self.output.push_str(&line);
+				self.output.push_str("\n");
+			}
 		}
 	}
 
 	pub struct Commands<'a>{
 		pub state: ListState,
-		pub commands: Vec<Command<'a>>
+		pub commands: Vec<Command<'a>>,
+		pub items: Vec<ListItem<'a>>
 	}
 
 	impl Commands<'_>{
@@ -50,16 +78,23 @@ pub mod commands{
 					Command::new(v)
 				})
 				.collect();
+			let items: Vec<ListItem> = commands.iter()
+				.map(|command| {
+					ListItem::new(command.command)
+						.style(Style::default())
+				})
+				.collect();
 
 			Commands {
 				state: ListState::default(),
-				commands: commands
+				commands: commands,
+				items: items
 			}
 		}
 
-		pub fn run(&mut self){
+		pub async fn run(&mut self){
 			for command in &mut self.commands {
-				command.run();
+				command.run().await;
 			}
 		}
 
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 use std::fs;
 use std::io;
-// use tui::{
-// 	backend::CrosstermBackend,
-// 	layout::{Constraint, Corner, Direction, Layout},
-// 	style::{Color, Modifier, Style},
-// 	text::{Span, Spans},
-// 	widgets::{Block, Borders, List, ListItem},
-// 	Terminal
-// };
-// use std::time::Duration;
-// use crossterm::event::{poll, read, Event, KeyCode, KeyEvent, KeyModifiers};
-// use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+use tui::{
+	backend::CrosstermBackend,
+	layout::{Constraint, Corner, Direction, Layout},
+	style::{Color, Modifier, Style},
+	text::{Span, Spans},
+	widgets::{Block, Borders, List},
+	Terminal
+};
+use std::time::Duration;
+use crossterm::event::{poll, read, Event, KeyCode, KeyEvent, KeyModifiers};
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
 
 mod configuration;
 mod commands;
@@ -22,59 +22,61 @@ async fn main() -> Result<(), io::Error> {
 		.expect("Something went wrong reading the file");
 
 	let mut commands = Commands::new(&contents);
-	commands.run();
+	commands.run().await;
 
-	// let stdout = io::stdout();
-	// let backend = CrosstermBackend::new(stdout);
-	// let mut terminal = Terminal::new(backend)?;
-
-	// enable_raw_mode().unwrap();
-	// loop{
-	// 	if poll(Duration::from_millis(100)).unwrap() {
-	// 		// It's guaranteed that `read` wont block, because `poll` returned
-	// 		// `Ok(true)`.
-	// 		match read().unwrap(){
-	// 			Event::Key(KeyEvent {
-	// 				code: KeyCode::Char('c'),
-	// 				modifiers: KeyModifiers::CONTROL
-	// 			}) => break,
-	// 			Event::Key(KeyEvent {
-	// 				code: KeyCode::Down,
-	// 				modifiers: KeyModifiers::NONE
-	// 			}) => {
-	// 				commands.next();
-	// 			},
-	// 			Event::Key(KeyEvent {
-	// 				code: KeyCode::Up,
-	// 				modifiers: KeyModifiers::NONE
-	// 			}) => {
-	// 				commands.previous();
-	// 			},
-	// 			_ => ()
-	// 		};
-	// 	} else {
-	// 		// Timeout expired, no `Event` is available
-	// 	}
-
-	// 	terminal.draw(|f| {
-	// 		let items = commands.items.clone();
-
-	// 		let list = List::new(items)
-	// 			.block(Block::default()
-	// 				.title("Micro Manage")
-	// 				.borders(Borders::ALL))
-	// 			.style(Style::default()
-	// 				.fg(Color::Black))
-	// 			.highlight_style(Style::default()
-	// 				.bg(Color::LightGreen)
-	// 				.add_modifier(Modifier::BOLD))
-	// 			.highlight_symbol(">>");
-
-	// 		let size = f.size();
-	// 		f.render_stateful_widget(list, size, &mut commands.state);
-	// 	})?;
+	// for command in commands.commands{
+	// 	print!("{}", command.output);
 	// }
 
-	// disable_raw_mode().unwrap();
+	let stdout = io::stdout();
+	let backend = CrosstermBackend::new(stdout);
+	let mut terminal = Terminal::new(backend)?;
+
+	enable_raw_mode().unwrap();
+	loop{
+		if poll(Duration::from_millis(100)).unwrap() {
+			// It's guaranteed that `read` wont block, because `poll` returned
+			// `Ok(true)`.
+			match read().unwrap(){
+				Event::Key(KeyEvent {
+					code: KeyCode::Char('c'),
+					modifiers: KeyModifiers::CONTROL
+				}) => break,
+				Event::Key(KeyEvent {
+					code: KeyCode::Down,
+					modifiers: KeyModifiers::NONE
+				}) => {
+					commands.next();
+				},
+				Event::Key(KeyEvent {
+					code: KeyCode::Up,
+					modifiers: KeyModifiers::NONE
+				}) => {
+					commands.previous();
+				},
+				_ => ()
+			};
+		} else {
+			// Timeout expired, no `Event` is available
+		}
+
+		terminal.draw(|f| {
+			let list = List::new(commands.items.clone())
+				.block(Block::default()
+					.title("Micro Manage")
+					.borders(Borders::ALL))
+				.style(Style::default()
+					.fg(Color::Black))
+				.highlight_style(Style::default()
+					.bg(Color::LightGreen)
+					.add_modifier(Modifier::BOLD))
+				.highlight_symbol(">>");
+
+			let size = f.size();
+			f.render_stateful_widget(list, size, &mut commands.state);
+		})?;
+	}
+
+	disable_raw_mode().unwrap();
 	Ok(())
 }
